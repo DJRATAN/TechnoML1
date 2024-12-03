c8a6f9163fbaea2f97f8aea903cff6b67d1a6074
Ratan Prajapati <ratanprajapati1242@gmail.com>
2024-12-03 22:18:28 +0530

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,6 +21,18 @@
 
   document.addEventListener('scroll', toggleScrolled);
   window.addEventListener('load', toggleScrolled);
+  /**
+   * Animation on scroll function and init
+   */
+  function aosInit() {
+    AOS.init({
+      duration: 600,
+      easing: 'ease-in-out',
+      once: true,
+      mirror: false
+    });
+  }
+  window.addEventListener('load', aosInit);
 
   /**
    * Mobile nav toggle
@@ -47,7 +59,38 @@
     });
 
   });
+/**
+   * Init isotope layout and filters
+   */
+document.querySelectorAll('.isotope-dlayout').forEach(function(isotopeItem) {
+  let layout = isotopeItem.getAttribute('data-layout') ?? 'masonry';
+  let filter = isotopeItem.getAttribute('data-default-filter') ?? '*';
+  let sort = isotopeItem.getAttribute('data-sort') ?? 'original-order';
 
+  let initIsotope;
+  imagesLoaded(isotopeItem.querySelector('.isotope-container'), function() {
+    initIsotope = new Isotope(isotopeItem.querySelector('.isotope-container'), {
+      itemSelector: '.isotope-item',
+      layoutMode: layout,
+      filter: filter,
+      sortBy: sort
+    });
+  });
+
+  isotopeItem.querySelectorAll('.isotope-filters li').forEach(function(filters) {
+    filters.addEventListener('click', function() {
+      isotopeItem.querySelector('.isotope-filters .filter-active').classList.remove('filter-active');
+      this.classList.add('filter-active');
+      initIsotope.arrange({
+        filter: this.getAttribute('data-filter')
+      });
+      if (typeof aosInit === 'function') {
+        aosInit();
+      }
+    }, false);
+  });
+
+});
   /**
    * Toggle mobile nav dropdowns
    */
